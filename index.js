7f4393a1564542982ecb3103c0589111c316a117
roney2003 <suryanshronak3@gmail.com>
2024-04-09 20:38:26 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const gitHubData =  {
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -16,6 +37,10 @@ app.get('/twitter', (req, res) => {
 
   app.get('/login',(req,res)=>{
     res.send('login page')
+  })
+
+  app.get('/github',(req,res)=>{
+    res.json(gitHubData)       // json ye function hai jo data ko json format me convert kar deta hai
   })
 
 app.listen(process.env.PORT, () => {
